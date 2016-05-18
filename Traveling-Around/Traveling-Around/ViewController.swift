a5a7778423015a6da848ffbd2e71bf2ae863e6c2
Fanglei <fanglei19900113@126.com>
2016-05-18 16:46:43 +0800

    establish project

---
+++ Traveling-Around/Traveling-Around/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Traveling-Around
+//
+//  Created by Fanglei on 16/5/18.
+//  Copyright © 2016年 Fanglei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
